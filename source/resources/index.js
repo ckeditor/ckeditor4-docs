3232fbe37389a1dadae308039d99219d15a0b7f0
Garry Yao <garry.yao@cksource.com>
2012-11-23 13:46:31 +0800

    Bring back the logo header, revert "Reverted small header in API docs due to scrolling issues."
    This reverts commit f34d828cc0932e869e98fb152fcb48a178ebb0f5.

--- source/resources/index.js
+++ source/resources/index.js
@@ -11,4 +11,20 @@ Ext.onReady( function() {
 				evt.preventDefault();
 
 	} );
+
+	// Various position fixes.
+	setTimeout( function() {
+		var north = Ext.getCmp( 'north-region' ),
+			search = Ext.getCmp( 'search-container' );
+
+		north.setHeight( 100 );
+		var header = north.child( 'container' );
+		header.setHeight( 73 );
+		search.getEl().setTop( 30 );
+
+		// Layout fix on resize.
+		header.on( 'resize', function() {
+			search.getEl().setTop( 30 );
+		} );
+	} );
 } );

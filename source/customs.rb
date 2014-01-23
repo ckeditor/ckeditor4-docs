d668bad3b9353f5a81f7e5d34341e65744c6d1f9
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2014-01-23 17:14:15 +0100

    Updated custom tags to the format supported by JSDuck 5.3.x.

--- source/customs.rb
+++ source/customs.rb
@@ -1,69 +1,39 @@
 # Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
 # For licensing, see LICENSE.html or http://ckeditor.com/license
 
-require "jsduck/meta_tag"
-require "jsduck/app"
-require "jsduck/doc_formatter"
+require "jsduck/tag/tag"
+require "jsduck/tag/boolean_tag"
+
+# See https://github.com/senchalabs/jsduck/wiki/Custom-tags
 
 module JsDuck::Tag
+	class License < Ignore
+		def initialize
+			@tagname = :ignore
+			@pattern = "license"
+		end
+	end
 
-  # Tags to be ignored in documentation.
+	class FileOverview < Ignore
+		def initialize
+			@tagname = :ignore
+			@pattern = "fileOverview"
+		end
+	end
 
-  class License < Ignore
-    def initialize
-      super()
-      @name = "license"
-    end
-  end
+	class Todo < BooleanTag
+		def initialize
+			@tagname = :todo
+			@pattern = "todo"
+			@repeatable = true
+		end
+	end
 
-  class FileOverview < Ignore
-    def initialize
-      super()
-      @name = "fileOverview"
-    end
-  end
-
-  # Do not ignore @todo like @license and @fileOverview.
-  # Just don't print anything.
-
-  class Todo < JsDuck::MetaTag
-    def initialize
-      @name = "todo"
-    end
-  end
-
-  # Temporary impl - will eventually print "see" section.
-
-  class See < JsDuck::MetaTag
-    def initialize
-      @name = "see"
-    end
-  end
-
-  # Custom @see tag.
-
-  # class See < JsDuck::MetaTag
-
-  #   def initialize
-  #     @name = "see"
-  #   end
-
-  #   def to_value( raw )
-  #     doc_formatter = DocFormatter.new( {}, {} )
-
-  #     raw.map do |link|
-  #       doc_formatter.create_magic_links( link )
-  #     end.compact
-  #   end
-
-  #   def to_html( links )
-  #     puts links
-  #     # <<-EOHTML
-  #     #   <h3 class="pa">See</h3>
-  #     #   <ul>
-  #     #     <li><a href="#{link}">#{link}</a></li>
-  #     #   </ul>
-  #     # EOHTML
-  #   end
-  # end
+	class See < Tag
+		def initialize
+			@tagname = :see
+			@pattern = "see"
+			@repeatable = true
+		end
+	end
 end

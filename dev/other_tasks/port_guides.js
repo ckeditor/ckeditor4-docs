f390e751fda61abcae7e17984473b2c746edcc20
m-turek <maciek.turek87@gmail.com>
2018-01-18 14:46:28 +0100

    Removed old documentation files which will be no longer used.

--- dev/other_tasks/port_guides.js
+++
@@ -1,9 +0,0 @@
-const portGuides = require( './guides/port' );
-const path = require( 'path' );
-
-portGuides( {
-	srcDir: 'guides',
-	dstDir: path.resolve( 'docs/guide' ),
-	assetsDir: path.resolve( 'docs/assets' ),
-	guidesConfig: require( '../../guides/guides.json' )
-} );

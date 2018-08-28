f8dc29c6c6b938ab6901eb30c492978f8a96f601
Mateusz Samsel <m.samsel@cksource.com>
2018-08-28 17:35:38 +0200

    Add script to extend default config in samples with custom options.

--- scripts/build-sdk.js
+++ scripts/build-sdk.js
@@ -6,6 +6,7 @@
 const path = require( 'path' );
 const fs = require( 'fs' );
 const buildCkeditor = require( './tools/build-ckeditor' );
+const updateConfigFile = require( './tools/update-config-file' );
 const promisify = require( './tools/promisify' );
 
 const destinationPath = path.join( process.cwd(), 'docs', 'sdk', 'examples', 'vendors' );
@@ -16,6 +17,10 @@ module.exports = makeFolder( destinationPath )
         dev: false
         } )
     )
+    .then( () => updateConfigFile( {
+        configFileSrc: path.join( process.cwd(), 'common-examples-config.json' ),
+        configFileDst: path.join( destinationPath, 'ckeditor', 'config.js' )
+    } ) )
     .catch( ( err ) => {
         process.exitCode = 1;
     } );
---
+++ scripts/tools/update-config-file.js
@@ -0,0 +1,47 @@
+/**
+ * @license Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+const fs = require( 'fs' );
+
+const promisify = require( './promisify.js' );
+
+module.exports = ( { configFileSrc, configFileDst } ) => new Promise( ( resolve, reject ) => {
+    let commonConfig = {};
+    promisify( fs.stat, fs )( configFileSrc )
+        .then( status => {
+            if ( status.isFile() ) {
+                return promisify( fs.readFile, fs)( configFileSrc );
+            } else {
+                reject();;
+            }
+        } )
+        .then( data => data.toString() )
+        .then( data => { 
+            commonConfig = JSON.parse( data );
+        } )
+        .then( () => promisify( fs.stat, fs )( configFileDst ) )
+        .then( status => {
+            if ( status.isFile() ) {
+                return promisify( fs.readFile, fs)( configFileDst );
+            } else {
+                reject();;
+            }
+        } )
+        .then( data => data.toString() )
+        .then( data => {
+            let insertedData = '\t// Custom config injected by \'update-config-file.js\' script.\n';
+            for ( const key in commonConfig ) {
+                if ( commonConfig.hasOwnProperty( key ) ) {
+                    insertedData += `\tconfig.${ key } = '${ commonConfig[ key ] }',\n`;
+                }
+            }
+            insertedData += '\t// End of injected config.\n';
+            return data.replace( /function\( config \) \{\n/, 'function( config ) {\n' + insertedData );
+        } )
+        .then( data => promisify( fs.writeFile, fs )( configFileDst, data, 'utf8' ) )
+        .then( () => {
+            resolve();
+        } );
+} );

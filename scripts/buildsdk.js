37cbe7e4aee10e5acc6c170520567de5576badb0
Mateusz Samsel <m.samsel@cksource.com>
2018-12-05 11:59:05 +0100

    Replace spaces with tabs in js scripts. Include editorconfig file to prevent simmilar situation in future.

--- scripts/buildsdk.js
+++ scripts/buildsdk.js
@@ -10,32 +10,32 @@ const updateConfigFile = require( './tools/updateconfigfile' );
 const chalk = require( 'chalk' );
 
 module.exports = new Promise( ( resolve, reject ) => {
-    let destinationPath;
+	let destinationPath;
 
-    getConfigSdkPath()
-        .then( configParts => { destinationPath = path.join( process.cwd(), 'docs', ...configParts, 'vendors' ) } )
-        .then( () => fs.ensureDir( destinationPath ) )
-        .then( () => buildCkeditor( {
-            destinationPath
-        } ) )
-        .then( () => updateConfigFile( {
-            configFileSrc: path.join( process.cwd(), 'commonconfig.json' ),
-            configFileDst: path.join( destinationPath, 'ckeditor', 'config.js' )
-        } ) )
-        .then( () => {
-            console.log( chalk.greenBright( 'Building CKEditor in presets complete.' ) );
-            resolve();
-        } )
-        .catch( ( err ) => {
-            process.exitCode = 1;
-            reject( err );
-        } );
+	getConfigSdkPath()
+		.then( configParts => { destinationPath = path.join( process.cwd(), 'docs', ...configParts, 'vendors' ) } )
+		.then( () => fs.ensureDir( destinationPath ) )
+		.then( () => buildCkeditor( {
+			destinationPath
+		} ) )
+		.then( () => updateConfigFile( {
+			configFileSrc: path.join( process.cwd(), 'commonconfig.json' ),
+			configFileDst: path.join( destinationPath, 'ckeditor', 'config.js' )
+		} ) )
+		.then( () => {
+			console.log( chalk.greenBright( 'Building CKEditor in presets complete.' ) );
+			resolve();
+		} )
+		.catch( ( err ) => {
+			process.exitCode = 1;
+			reject( err );
+		} );
 } );
 
 function getConfigSdkPath() {
-    return fs.readJson( path.join( process.cwd(), 'umberto.json' ) )
-        .then( data => {
-            const sdkGroup = data.groups.find( g => g.id === 'sdk' );
-            return sdkGroup.sourceDir.split( '/' );
-        } );
+	return fs.readJson( path.join( process.cwd(), 'umberto.json' ) )
+		.then( data => {
+			const sdkGroup = data.groups.find( g => g.id === 'sdk' );
+			return sdkGroup.sourceDir.split( '/' );
+		} );
 }
--- scripts/tools/buildckeditor.js
+++ scripts/tools/buildckeditor.js
@@ -9,41 +9,41 @@ const chalk = require( 'chalk' );
 const { spawn } = require( 'child_process' );
 
 module.exports = ( { destinationPath } ) => new Promise( ( resolve, reject ) => {
-    if ( !destinationPath ) {
-        reject( new Error( 'There is no destination path given.' ) )
-    }
-    return fs.remove( path.join( destinationPath, 'ckeditor' ) )
-        .then( () => buildAndCopyCkeditor( destinationPath ) )
-        .then( () => {
-            resolve();
-        } );
+	if ( !destinationPath ) {
+		reject( new Error( 'There is no destination path given.' ) )
+	}
+	return fs.remove( path.join( destinationPath, 'ckeditor' ) )
+		.then( () => buildAndCopyCkeditor( destinationPath ) )
+		.then( () => {
+			resolve();
+		} );
 } );
 
 function buildAndCopyCkeditor( destinationPath ) {
-    console.log( chalk.greenBright( 'Building CKEditor...' ) );
-    return buildCkeditor()
-        .then( () => copyCkeditor( destinationPath ) );
+	console.log( chalk.greenBright( 'Building CKEditor...' ) );
+	return buildCkeditor()
+		.then( () => copyCkeditor( destinationPath ) );
 }
 
 function buildCkeditor() {
-    return new Promise( ( resolve, reject ) => {
-        const build = spawn( path.join( process.cwd(), 'repos', 'ckeditor-presets', 'build.sh' ), [ 'standard', 'all' ] );
-        build.stdout.on( 'data', data => { process.stdout.write( data.toString() ) } );
-        build.on( 'close', resolve );
-        build.on( 'error', reject );
-    } );
+	return new Promise( ( resolve, reject ) => {
+		const build = spawn( path.join( process.cwd(), 'repos', 'ckeditor-presets', 'build.sh' ), [ 'standard', 'all' ] );
+		build.stdout.on( 'data', data => { process.stdout.write( data.toString() ) } );
+		build.on( 'close', resolve );
+		build.on( 'error', reject );
+	} );
 }
 
 function copyCkeditor( destinationPath ) {
-    return getCkeditorVersion( process.cwd() )
-        .then( ckeditorVersion => fs.copy(
-                path.join( process.cwd(), 'repos', 'ckeditor-presets', 'build', ckeditorVersion, 'standard-all', 'ckeditor' ),
-                path.join( destinationPath, 'ckeditor' )
-            )
-        );
+	return getCkeditorVersion( process.cwd() )
+		.then( ckeditorVersion => fs.copy(
+				path.join( process.cwd(), 'repos', 'ckeditor-presets', 'build', ckeditorVersion, 'standard-all', 'ckeditor' ),
+				path.join( destinationPath, 'ckeditor' )
+			)
+		);
 }
 
 function getCkeditorVersion( basePath ) {
-    return fs.readJson( path.join( basePath, 'repos', 'ckeditor-presets', 'ckeditor', 'package.json' ) )
-        .then( data => data.version );
+	return fs.readJson( path.join( basePath, 'repos', 'ckeditor-presets', 'ckeditor', 'package.json' ) )
+		.then( data => data.version );
 }

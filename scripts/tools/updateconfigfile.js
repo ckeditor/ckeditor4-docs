a08af3254d3ee95399b1fd718341a5294812eadd
Mateusz Samsel <m.samsel@cksource.com>
2018-12-12 15:34:48 +0100

    Add support fore placeholder in commonconfig.

--- scripts/tools/updateconfigfile.js
+++ scripts/tools/updateconfigfile.js
@@ -3,15 +3,18 @@
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 
+'use strict';
+
 const fs = require( 'fs-extra' );
+const path = require( 'path' );
 
 module.exports = ( { configFileSrc, configFileDst } ) => new Promise( ( resolve, reject ) => {
-	Promise.all( [ fs.readJson( configFileSrc ), fs.readFile( configFileDst ).then( data => data.toString() ) ] )
-		.then( ( [ srcConfig, dstFile ] ) => {
+	Promise.all( [ fs.readJson( configFileSrc ), fs.readFile( configFileDst ).then( data => data.toString() ), fs.readJson( path.join( process.cwd(), 'umberto.json' ) ) ] )
+		.then( ( [ srcConfig, dstFile, config ] ) => {
 			let insertedData = '\t// Common config injected by examples building script.\n';
 			for ( const key in srcConfig ) {
 				if ( srcConfig.hasOwnProperty( key ) && typeof srcConfig[ key ] === 'string' ) {
-					insertedData += `\tconfig.${ key } = '${ srcConfig[ key ] }';\n`;
+					insertedData += `\tconfig.${ key } = '${ replacePlaceholders( srcConfig[ key ], config.variables ) }';\n`;
 				}
 			}
 			insertedData += '\t// End of injected config.\n';
@@ -23,3 +26,22 @@ module.exports = ( { configFileSrc, configFileDst } ) => new Promise( ( resolve,
 		} )
 		.catch( err => reject( err ) );
 } );
+
+function replacePlaceholders( inputString, variables ) {
+	const macros = new Set( inputString.match( /\{%\s?[A-Z_]+?\s?%\}/g ) );
+	let outputText = inputString;
+	if ( !macros.size ) {
+		return inputString;
+	}
+	if ( !variables ) {
+		throw new Error( `Variables are not defined in umberto config, however there was detected macro usage in customconfig.js` );
+	}
+	macros.forEach( macro => {
+		const name = macro.match( /\{%\s?([A-Z_]+)\s?%\}/ )[ 1 ]
+		if ( variables[ name ] === undefined ) {
+			throw new Error( `Macro "${ macro }" used in customconfig.js doesn't have corresponding variable in configuration.` );
+		}
+		outputText = outputText.replace( new RegExp( macro, 'g' ), variables[ name ] );
+	} );
+	return outputText;
+}

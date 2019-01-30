4d96dacbe299a04115637b1a53c456fa460dfdb1
Kajetan Litwinowicz <k.litwinowicz@cksource.com>
2019-01-30 14:25:27 +0100

    Updated editor-types sample, removed angular directive controls.

--- angular/src/app/editor-types/editor-types.component.ts
+++ angular/src/app/editor-types/editor-types.component.ts
@@ -13,39 +13,13 @@ import { Component } from '@angular/core';
 export class EditorTypesComponent {
 	public isReadOnly = false;
 	public editorData =
-		`<p>Getting used to an entirely different culture can be challenging.
-While it’s also nice to learn about cultures online or from books, nothing comes close to experiencing cultural diversity in person.
-You learn to appreciate each and every single one of the differences while you become more culturally fluid.</p>`;
-
-	isHidden = false;
-
-	isRemoved = false;
+		`<p>Hello world!</p>`;
 
 	inline = false;
 
 	public componentEvents: string[] = [];
 
-	toggleDisableEditors() {
-		this.isReadOnly = !this.isReadOnly;
-	}
-
 	changeEditorType() {
 		this.inline = !this.inline;
-	}
-
-	onReady( editor: any, EditorName: string ): void {
-		console.log( `${EditorName} editor is ready.` );
-	}
-
-	onChange( event: any, EditorName: string ): void {
-		console.log( `${EditorName} editor model changed.` );
-	}
-
-	onFocus( event: any, EditorName: string ): void {
-		console.log( `Focused ${EditorName.toLowerCase()} editing view.` );
-	}
-
-	onBlur( event: any, EditorName: string ): void {
-		console.log( `Blurred ${EditorName.toLowerCase()} editing view.` );
 	}
 }
